2ecad108b4053e8ffa094df7c8ec485f98aebaf2
Adarsh Singh <adarshsinghhajipur5@gmail.com>
2024-11-16 14:00:41 +0530

    v 2.0.0

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const courseBuyBtn1 = document.getElementById('course-buy-btn');
+    const payPageBackBtn = document.getElementById('pay-page-back-btn');
+
+    if (courseBuyBtn1) {
+        // BUY NOW BTN -> PAYMENT PAGE
+        courseBuyBtn1.addEventListener('click', () => {
+            window.location.href = 'payment.html';
+        });
+    }
+
+    if (payPageBackBtn) {
+        // MAIN PAGE <- PAYMENT PAGE
+        payPageBackBtn.addEventListener('click', () => {
+            window.location.href = 'index.html';
+        });
+    }
+});
